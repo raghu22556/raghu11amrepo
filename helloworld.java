216e7343194e12da6347715acb7127ad157d6605
raghu22556 <nani.raghu007@gmail.com>
2021-09-30 17:58:25 +0530

    new changes added in helloworld

--- helloworld.java
+++ helloworld.java
@@ -6,4 +6,12 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
     }
 }[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[B[B[B[B[B[B[C
-
+/* This is a simple Java program. 
+FileName : "HelloWorld.java". */
+public class HelloWorld {
+  //Your program begins by calling the main(). 
+  //Prints "Hello, World" to the terminal window. 
+  public static void main(String args[]) {
+    System.out.println("Hello World");
+  }
+}
